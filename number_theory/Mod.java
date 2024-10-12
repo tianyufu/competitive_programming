91a581f784fc95d1ab152b97467deb2ebff847c2
tianyufu <futianyu49@gmail.com>
2024-10-12 09:08:00 -0700

    add nck

--- number_theory/Mod.java
+++ number_theory/Mod.java
@@ -16,4 +16,17 @@ class Mod {
         }
         return res;
     }
+
+    long nck(int n, int k) {
+        if (k < 0 || k > n) {
+            return 0;
+        }
+
+        long result = 1;
+        for (int i = 1; i <= k; i++) {
+            result = (result * (n - k + i)) % MOD;
+            result = (result * inv(i)) % MOD;
+        }
+        return result;
+    }
 }
