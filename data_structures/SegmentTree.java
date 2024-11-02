16567525ffb59d0c92803851d1f2b858cfb02945
tianyufu <futianyu49@gmail.com>
2024-11-02 14:21:09 -0700

    More segment tree

--- data_structures/SegmentTree.java
+++ data_structures/SegmentTree.java
@@ -56,4 +56,142 @@ class SegmentTree {
             t[v] = func(t[v * 2], t[v * 2 + 1]);
         }
     }
+
+    /******************************************/
+    /**** addtion on segments, point query ****/
+    private void build(int[] a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v * 2, tl, tm);
+            build(a, v * 2 + 1, tm + 1, tr);
+            t[v] = 0;
+        }
+    }
+
+    private void update(int v, int tl, int tr, int l, int r, int add) {
+        if (l > r) return;
+        if (l == tl && r == tr) {
+            t[v] += add;
+        } else {
+            int tm = (tl + tr) / 2;
+            update(v * 2, tl, tm, l, Math.min(r, tm), add);
+            update(v * 2 + 1, tm + 1, tr, Math.max(l, tm + 1), r, add);
+        }
+    }
+
+    int get(int v, int tl, int tr, int pos) {
+        if (tl == tr) return t[v];
+        int tm = (tl + tr) / 2;
+        if (pos <= tm) {
+            return t[v] + get(v * 2, tl, tm, pos);
+        } else {
+            return t[v] + get(v * 2 + 1, tm + 1, tr, pos);
+        }
+    }
+    /******************************************/
+
+
+    /*********************************************/
+    /**** assignment on segments, point query ****/
+    int[] marked = new int[n * 4];
+
+    private void build(int[] a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v * 2, tl, tm);
+            build(a, v * 2 + 1, tm + 1, tr);
+            t[v] = 0;
+        }
+    }
+
+    private void push(int v) {
+        if (marked[v]) {
+            t[v * 2] = t[v];
+            t[v * 2 + 1] = t[v];
+            marked[v * 2] = true;
+            marked[v * 2 + 1] = true;
+            marked[v] = false;
+        }
+    }
+
+    private void update(int v, int tl, int tr, int l, int r, int newVal) {
+        if (l > r) return;
+        if (l == tl && r == tr) {
+            t[v] = newVal;
+            marked[v] = true;
+        } else {
+            push(v);
+            int tm = (tl + tr) / 2;
+            update(v * 2, tl, tm, l, Math.min(r, tm), newVal);
+            update(v * 2 + 1, tm + 1, tr, Math.max(l, tm + 1), r, newVal);
+        }
+    }
+
+    int get(int v, int tl, int tr, int pos) {
+        if (tl == tr) return t[v];
+        push(v);
+        int tm = (tl + tr) / 2;
+        if (pos <= tm) {
+            return get(v * 2, tl, tm, pos);
+        } else {
+            return get(v * 2 + 1, tm + 1, tr, pos);
+        }
+    }
+    /*********************************************/
+
+
+    /*****************************************/
+    /**** Adding on segments, range query ****/
+    int[] lazy = new int[n * 4];
+
+    private void build(int[] a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v * 2, tl, tm);
+            build(a, v * 2 + 1, tm + 1, tr);
+            t[v] = func(t[v * 2], t[v * 2 + 1]);
+        }
+    }
+
+    private void push(int v) {
+        t[v * 2] += lazy[v];
+        t[v * 2 + 1] += lazy[v];
+        lazy[v * 2] += lazy[v];
+        lazy[v * 2 + 1] += lazy[v];
+
+        lazy[v] = 0;
+    }
+
+    private void update(int v, int tl, int tr, int l, int r, int addend) {
+        if (l > r) return;
+        if (l == tl && r == tr) {
+            t[v] += addend;
+            lazy[v] += addend;
+        } else {
+            push(v);
+            int tm = (tl + tr) / 2;
+            update(v * 2, tl, tm, l, Math.min(r, tm), addend);
+            update(v * 2 + 1, tm + 1, tr, Math.max(l, tm + 1), r, addend);
+            t[v] = func(t[v * 2], t[v * 2 + 1]);
+        }
+    }
+
+    int query(int v, int tl, int tr, int l, int r) {
+        if (l > r) return init;
+        if (l == tl && tr == r) return t[v];
+        push(v);
+        int tm = (tl + tr) / 2;
+        return func(
+                query(v * 2, tl, tm, l, Math.min(r, tm)), 
+                query(v * 2 + 1, tm + 1, tr, Math.max(l, tm + 1), r)
+            );
+    }
+    /*********************************************/
+
 }
